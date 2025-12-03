bb8143998bd7aa6b52a3264634ef20378178ecce
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2025-12-03 18:36:05 +0300

    ui-kit: better socials icons + allow custom classes on socials component

--- src/ui-kit/socials/socials.tsx
+++ src/ui-kit/socials/socials.tsx
@@ -1,7 +1,10 @@
+'use server';
+
+import composeClassName from '@src/helpers/compose-class-name';
 import './socials.scss';
 
-export default function Socials() {
-  return <div className='socials'>
+export default async function Socials({ customClass }: { customClass?: string }) {
+  return <div className={composeClassName('socials', customClass)}>
     <img className='socials-icon' src='icons/viber.webp' alt='Иконка' />
     <img className='socials-icon' src='icons/whatsapp.webp' alt='Иконка' />
     <img className='socials-icon' src='icons/telegram.webp' alt='Иконка' />
