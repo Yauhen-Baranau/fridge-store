fd6bdc985e2ea790fc6190604ca61e32cbf2fc4f
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2025-12-05 20:48:05 +0300

    header: allow to move to main page on image click

--- src/app/components/header/header.tsx
+++ src/app/components/header/header.tsx
@@ -5,15 +5,13 @@ import styles from './header.module.scss';
 import composeClassName from '@src/helpers/compose-class-name';
 import Socials from '@src/ui-kit/socials/socials';
 import { contactInfo } from '@src/constants/contact-info';
+import Link from 'next/link';
 
 export default async function Header({ customClass }: { customClass?: string }) {
-  const address = 'г. Минск, ул. Домбровская, 9';
-  const openHours = 'Пн-Вс с 09.00 до 21.00';
-  const phoneNumber= '+375 (33) 364-18-81';
-  const email = 'online888bazar@gmail.com';
-
   return <header className={composeClassName(styles.header, customClass)}>
-    <img className={styles.logo} src='logo.webp' alt='Логотип' width={227} height={40} />
+    <Link href='/'>
+      <img className={styles.logo} src='logo.webp' alt='Логотип' width={227} height={40} />
+    </Link>
     <div className={styles['open-hours']}>
       <div className={styles['icon-text-pair']}>
         <img className={styles['header-icon']} src='icons/location.webp' alt='Иконка' />
