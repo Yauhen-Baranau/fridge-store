5e6afec6314d1b20cef1407d53bc59d5ad6c0928
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2025-12-05 20:10:57 +0300

    about us block: fix learn more button upper margin

--- src/app/components/about-us/about-us.tsx
+++ src/app/components/about-us/about-us.tsx
@@ -16,13 +16,12 @@ export default async function AboutUs() {
         Мы ценим ваше время, поэтому выезжаем точно по договоренности, <span className={styles.bold}>диагностируем быстро</span> и устраняем неполадки
         уже <span className={styles.bold}>в день обращения</span>. Мы используем <span className={styles.bold}>только оригинальные запчасти</span>, предоставляем гарантию на все виды работ
         и всегда объясняем клиенту суть проблемы простыми словами.<br /><br />
-        <span className={styles.bold}>Наша цель</span> — не просто отремонтировать технику, а сделать так, чтобы вы больше не волновались о её работе.<br /><br /><br />
-        {/* TO DO: fix margin */}
-        <Link href='https://google.com'>
-          <Button text='Подробнее' style='text-only' />
-        </Link>
+        <span className={styles.bold}>Наша цель</span> — не просто отремонтировать технику, а сделать так, чтобы вы больше не волновались о её работе.
       </p>
-      <img src='about-us-photo.webp' width={470} height={522} alt='Специалист по ремонту холодильников' />
+      <Link className={styles['about-us-link']} href='https://google.com'>
+        <Button text='Подробнее' style='text-only' />
+      </Link>
     </div>
+    <img className={styles['about-us-image']} src='about-us-photo.webp' width={470} height={522} alt='Специалист по ремонту холодильников' />
   </section>
 }
