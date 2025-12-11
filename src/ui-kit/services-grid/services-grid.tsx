26386c13174860ca413da400c615ab9ff1ae62e3
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2025-12-11 10:56:39 +0300

    ui-kit: services grid: make service price optional

--- src/ui-kit/services-grid/services-grid.tsx
+++ src/ui-kit/services-grid/services-grid.tsx
@@ -3,13 +3,12 @@ import styles from './services-grid.module.scss';
 import Image from 'next/image';
 import Button from '../button/button';
 import Link from 'next/link';
-import { Routes } from '@constants/routes';
 
 interface Service {
   imagePath: string,
   label: string,
-  price: number,
-  redirectTo: Routes,
+  price?: number,
+  redirectTo: string,
 }
 
 export default function ServicesGrid({ services }: { services: Array<Service> }) {
@@ -20,7 +19,7 @@ export default function ServicesGrid({ services }: { services: Array<Service> })
         <h4 className={styles['service-label']}>{service.label}</h4>
         <span className={styles['with-parts']}>С учетом запчастей</span>
         <div className={styles['service-footer']}>
-          <span className={styles.price}>от {service.price} руб.</span>
+          {service.price && <span className={styles.price}>от {service.price} руб.</span>}
           <Link className={styles['service-learn-more']} href={service.redirectTo}>
             <Button customClass={styles['service-learn-more-button']} text='Подробнее' style='text-only' />
           </Link>
