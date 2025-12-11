2b711aaa9303256273a875b06b05db0c72159734
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2025-12-11 15:28:01 +0300

    ui-kit: accordion: toggle button

--- src/ui-kit/accordion/accordion.tsx
+++ src/ui-kit/accordion/accordion.tsx
@@ -3,24 +3,51 @@
 import './accordion.scss';
 import { useState } from 'react';
 import composeClassName from '@src/helpers/compose-class-name';
+import Button from '../button/button';
 
 export default function Accordion({
-  children,
+  toggleAreaContent,
+  content,
+  buttonStyle = 'arrow',
   customClass,
 }: {
-  children: React.ReactNode,
+  toggleAreaContent: React.ReactNode,
+  content: React.ReactNode,
+  buttonStyle?: 'arrow' | 'plus',
   customClass?: string,
 }) {
   const [isRevealed, setIsRevealed] = useState(false);
   const toggleRevealed = () => setIsRevealed(!isRevealed);
 
-  return <div className={composeClassName('outer-accordion-wrapper', customClass)}>
-    <div className='accordion-toggle-area' onClick={toggleRevealed}>
-      toggle
+  const getToggleButton = (buttonStyle: string) => {
+    switch (buttonStyle) {
+      case 'arrow':
+        return <Button
+          customClass={'accordion-toggle-arrow-button'}
+          icon={{ path: '/icons/thick-arrow-up.svg', width: 24, height: 24 }}
+        />;
+      case 'plus':
+        return <></>;
+      default:
+        return <></>;
+    }
+  }
+
+  return <div className={'outer-accordion-wrapper'}>
+    <div
+      className={composeClassName(
+        'accordion-toggle-area',
+        isRevealed && 'revealed',
+        customClass
+      )}
+      onClick={toggleRevealed}
+    >
+      {toggleAreaContent}
+      {getToggleButton(buttonStyle)}
     </div>
     <div className={composeClassName(
       'content-wrapper',
       isRevealed && 'revealed'
-    )}>{children}</div>
+    )}>{content}</div>
   </div>
 }
