eee8f13e24b84edcd080f12d11db99f3837d83ff
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2025-12-14 18:34:10 +0300

    ui-kit: form: validation

--- src/ui-kit/form/form.tsx
+++ src/ui-kit/form/form.tsx
@@ -5,22 +5,21 @@ import './form.scss';
 import { useRef, useState } from 'react';
 import Button from '../button/button';
 import useClickOutsideListener from '@src/hooks/use-click-outside-listener';
+import { Validator } from './validators';
 
-interface FormFieldConfig {
+interface FormFieldConfig<T = string> {
   type: 'text' | 'tel' | 'textarea';
   name: string,
   placeholder?: string,
-  maxLength?: number,
-  validatorRegExp?: RegExp;
-  validationErrorText?: string;
-}
-
-interface FormValue {
-  [key: string]: unknown;
+  validators?: Array<Validator<T>>,
 }
 
 interface FormConfig {
   fieldConfigs: Array<FormFieldConfig>;
+}
+
+interface FormValue {
+  [key: string]: unknown;
 }
 
 export default function Form({
@@ -47,6 +46,18 @@ export default function Form({
   const formRef = useRef<HTMLFormElement>(null);
   useClickOutsideListener(formRef, resetActiveInput);
 
+  const [submitAttempted, setSubmitAttempted] = useState(false);
+
+  const getValidationError = <T,>(fieldConfig: FormFieldConfig<T>) => {
+    const fieldValue = formValue[fieldConfig.name] as T;
+    const validationErrors = (fieldConfig.validators ?? [])
+      .map(validator => validator(fieldValue))
+      .filter(Boolean);
+    return validationErrors[0];
+  };
+  const validateField = (fieldConfig: FormFieldConfig) => !getValidationError(fieldConfig);
+  const validateForm = () => (config.fieldConfigs.every(fieldConfig => validateField(fieldConfig)));
+
   return <form
     ref={formRef}
     className={composeClassName('form', customClass)}
@@ -57,25 +68,38 @@ export default function Form({
       return <div
         key={index}
         className={composeClassName(
-          'input-wrapper',
-          activeInputName === fieldConfig.name && 'active-input',
-          !!formValue[fieldConfig.name] && 'non-empty-input'
+          'field-wrapper',
+          activeInputName === fieldConfig.name && 'active-field',
+          !!formValue[fieldConfig.name] && 'non-empty-field',
+          submitAttempted && !validateField(fieldConfig) && 'invalid-field'
         )}
         onClick={e => {
           e.stopPropagation();
           setActiveInputName(fieldConfig.name);
         }}
       >
-        <input
-          className='input'
-          type={fieldConfig.type}
-          name={fieldConfig.name}
-          onInput={e => setFormValueField(fieldConfig.name, (e.target as HTMLInputElement).value)}
-        />
+        <div className='input-wrapper'>
+          <input
+            className='input'
+            type={fieldConfig.type}
+            name={fieldConfig.name}
+            onInput={e => setFormValueField(fieldConfig.name, (e.target as HTMLInputElement).value)}
+          />
+        </div>
         {fieldConfig.placeholder && <span className='placeholder'>{fieldConfig.placeholder}</span>}
+        {(() => {
+          if (!submitAttempted) {
+            return <></>;
+          }
+          const validationError = getValidationError(fieldConfig);
+          return validationError && <span className={'error-message'}>{validationError}</span>
+        })()}
       </div>
     })}
     {preSubmitButtonContent}
-    <Button text={submitButtonText} onClick={() => submitCallback(formValue)} />
+    <Button text={submitButtonText} onClick={() => {
+      setSubmitAttempted(true);
+      validateForm() && submitCallback(formValue);
+    }} />
   </form>;
 }
---
+++ src/ui-kit/form/validators.ts
@@ -0,0 +1,30 @@
+// if validation fails, the validator will return the error message
+// otherwise, the validator will return an empty string
+// inspired by Angular's reactive forms
+export type Validator<T> = (v: T) => string;
+
+const validatorFactory = <T>(
+  validatorFunction: (v: T) => boolean,
+  errorMessage: string
+): Validator<T> => {
+  return (v: T) => validatorFunction(v) ? '' : errorMessage;
+}
+
+export const Validators = {
+  required: validatorFactory<unknown>(
+    v => !!v,
+    'Это поле необходимо заполнить'
+  ),
+  minLength: (length: number) => validatorFactory<string>(
+    (value: string) => value.length >= length,
+    'Количество символов меньше допустимого'
+  ),
+  maxLength: (length: number) => validatorFactory<string>(
+    (value: string) => value.length <= length,
+    'Количество символов превышает допустимое'
+  ),
+  pattern: (pattern: RegExp) => validatorFactory<string>(
+    (value: string) => pattern.test(value),
+    'Проверьте корректность внесенных данных'
+  ),
+};
