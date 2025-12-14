7899c0a29955c937a67a3dd880f00d862b25f41d
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2025-12-14 15:13:25 +0300

    hooks: click outside listener hook

---
+++ src/hooks/use-click-outside-listener.ts
@@ -0,0 +1,13 @@
+import { RefObject, useEffect } from "react";
+
+export default function useClickOutsideListener(ref: RefObject<HTMLElement | null>, handler: () => void) {
+  useEffect(() => {
+    const listener = (e: Event) => {
+      if (!ref.current?.contains(e.target as Node)) {
+        handler();
+      }
+    }
+    window.addEventListener('click', listener);
+    return () => window.removeEventListener('click', listener);
+  }, []);
+}
