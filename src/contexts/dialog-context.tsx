7956a14549ab55538be7f0d7fee427e5cafbeb94
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2025-12-15 13:17:09 +0300

    dialog context

---
+++ src/contexts/dialog-context.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import Dialog from "@ui-kit/dialog/dialog";
+import { createContext, useRef, useState } from "react";
+
+interface DialogContextProps {
+  showDialog: (content?: React.ReactNode) => void,
+  setDialogContent: (content: React.ReactNode) => void,
+  closeDialog: () => void,
+}
+
+export const DialogContext = createContext<DialogContextProps>({
+  showDialog: () => {},
+  setDialogContent: () => {},
+  closeDialog: () => {},
+});
+
+export const DialogContextProvider = ({
+  children
+}: {
+  children: React.ReactNode
+}) => {
+  const [dialogContent, setDialogContent] = useState<React.ReactNode>(<></>);
+  const dialogRef = useRef<HTMLDialogElement>(null);
+
+  return <DialogContext.Provider value={{
+    showDialog: (content?: React.ReactNode) => {
+      content && setDialogContent(content);
+      dialogRef?.current?.showModal();
+    },
+    setDialogContent,
+    closeDialog: () => dialogRef?.current?.close(),
+  }}>
+    {children}
+    <Dialog dialogRef={dialogRef}>{dialogContent}</Dialog>
+  </DialogContext.Provider>
+}
