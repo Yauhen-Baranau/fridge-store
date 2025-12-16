9530d694ac90ea24bc98d9b8e7ab51b1cd0ffc4f
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2025-12-16 18:13:39 +0300

    useResponsive hook: adjust behaviour

--- src/hooks/use-responsive.ts
+++ src/hooks/use-responsive.ts
@@ -4,10 +4,11 @@ export default function useResponsive() {
   const desktopToIpadBreakpoint = 1170;
   const ipadToMobileBreakpoint = 834;
 
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const [windowWidth, setWindowWidth] = useState(1280);
   useEffect(() => {
     const listener = () => setWindowWidth(window.innerWidth);
     window.addEventListener('resize', listener);
+    window.dispatchEvent(new Event('resize')); // can't use window in useState default value
     return () => window.removeEventListener('resize', listener);
   }, []);
 
