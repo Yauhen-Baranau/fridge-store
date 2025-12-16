fb7044f0c73131879ec38443efb9b9c118df33b9
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2025-12-16 11:39:54 +0300

    reviews page: add more mock reviews

--- src/app/reviews/page.tsx
+++ src/app/reviews/page.tsx
@@ -64,8 +64,23 @@ export default function ReviewsPage() {
           { path: '/water-bottles.webp', width: 570, height: 382 },
           { path: '/water-bottles.webp', width: 570, height: 382 },
         ],
-        timestamp: 0,
-      }
+        reviewTimestamp: 1763240400000,
+      },
+      {
+        reviewerName: 'Анна Ковальчук',
+        rating: 3,
+        comment: 'Обратилась в эту компанию впервые, заказывала генеральную уборку квартиры. Приехали два клинера, работали около 5-6 часов. Использовали профессиональную химию Grass, отчистили даже застарелые пятна на кухне. Очень довольна результатом, всё блестит!',
+        reviewTimestamp: 0,
+      },
+      {
+        reviewerName: 'Анна Ковальчук',
+        rating: 4,
+        comment: 'Обратилась в эту компанию впервые, заказывала генеральную уборку квартиры. Приехали два клинера, работали около 5-6 часов. Использовали профессиональную химию Grass, отчистили даже застарелые пятна на кухне. Очень довольна результатом, всё блестит!',
+        images: [
+          { path: '/water-bottles.webp', width: 570, height: 382 },
+        ],
+        reviewTimestamp: 1668546000000,
+      },
     ].map(reviewListItemFactory)} />
     <BackgroundSnowflake width={613} height={595} right={43} top={909} rotation={-30} color='light-blue' />
     <BackgroundSnowflake width={341} height={331} right={43} top={909} rotation={-30} color='light-blue' />
