f843ac1b329b6083fd2603a92e3409b48cffe3ff
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2025-12-16 16:52:07 +0300

    responsive: useResponsive hook

---
+++ src/hooks/use-responsive.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export default function useResponsive() {
+  const desktopToIpadBreakpoint = 1170;
+  const ipadToMobileBreakpoint = 834;
+
+  const [windowWidth, setWindowWidth] = useState(0);
+  useEffect(() => {
+    const listener = () => setWindowWidth(window.innerWidth);
+    window.addEventListener('resize', listener);
+    return () => window.removeEventListener('resize', listener);
+  }, []);
+
+  return {
+    isDesktop: windowWidth > desktopToIpadBreakpoint,
+    isIpad: windowWidth > ipadToMobileBreakpoint && windowWidth <= desktopToIpadBreakpoint,
+    isMobile: windowWidth <= ipadToMobileBreakpoint,
+  };
+}
