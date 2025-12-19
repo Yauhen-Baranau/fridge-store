d495c7f36e4ce22e72a27780915323e9dda59908
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2025-12-19 23:01:12 +0300

    responsive: navigation component (ipad)

--- src/ui-kit/navigation/navigation.tsx
+++ src/ui-kit/navigation/navigation.tsx
@@ -8,132 +8,84 @@ import List from '@ui-kit/list/list';
 import Link from 'next/link';
 import { Routes } from '@src/constants/routes';
 import { useHrefHelper } from '@contexts/href-context';
+import useResponsive from '@hooks/use-responsive';
 
 export default function Navigation({ customClass }: { customClass?: string }) {
   const { getCategoryHref, getSubcategoryHref } = useHrefHelper();
+  const { isDesktop } = useResponsive();
 
   return <nav className={composeClassName('nav', customClass)}>
     <List
       customClass='nav-list'
-      direction='horizontal'
-      nestedItemsStyle='popup'
+      direction={isDesktop ? 'horizontal' : 'vertical'}
+      nestedItemsStyle={isDesktop ? 'popup' : 'accordion'}
       items={[
         {
-          content: <Link href={getCategoryHref('1')}>УСЛУГИ ПО РЕМОНТУ ХОЛОДИЛЬНИКОВ</Link>,
-          icon: {
-            path: '/icons/chevron-down.svg',
-            width: 10,
-            height: 7,
-            position: 'after',
-          },
+          content: <Link href={getCategoryHref('1')}>Услуги по ремонту холодильников</Link>,
+          icon: { path: '/icons/chevron-down.svg', width: 10, height: 7, position: 'after' },
           subItems: [
             {
-              content: <Link href={getSubcategoryHref('1')}>ЗАМЕНА И РЕМОНТ КОМПОНЕНТОВ СИСТЕМЫ ОХЛАЖДЕНИЯ</Link>,
-              icon: {
-                path: '/icons/square-small.svg',
-                width: 14,
-                height: 14,
-              },
+              content: <Link href={getSubcategoryHref('1')}>Замена и ремонт компонентов системы охлаждения</Link>,
+              icon: { path: '/icons/square-small.svg', width: 14, height: 14 },
             },
             {
-              content: <Link href={getSubcategoryHref('2')}>РЕМОНТА И ЗАМЕНА ЭЛЕКТРИЧЕСКИХ КОМПОНЕНТОВ</Link>,
-              icon: {
-                path: '/icons/square-small.svg',
-                width: 14,
-                height: 14,
-              },
+              content: <Link href={getSubcategoryHref('2')}>Ремонт и замена электрических компонентов</Link>,
+              icon: { path: '/icons/square-small.svg', width: 14, height: 14 },
             },
             {
-              content: <Link href={getSubcategoryHref('3')}>ЗАМЕНА ЭЛЕМЕНТОВ СИСТЕМЫ NO FROST</Link>,
-              icon: {
-                path: '/icons/square-small.svg',
-                width: 14,
-                height: 14,
-              },
+              content: <Link href={getSubcategoryHref('3')}>Замена элементов системы No Frost</Link>,
+              icon: { path: '/icons/square-small.svg', width: 14, height: 14 },
             },
             {
-              content: <Link href={getSubcategoryHref('4')}>ЗАМЕНА И РЕМОНТ МЕХАНИЧЕСКИХ УЗЛОВ</Link>,
-              icon: {
-                path: '/icons/square-small.svg',
-                width: 14,
-                height: 14,
-              },
+              content: <Link href={getSubcategoryHref('4')}>Замена и ремонт механических узлов</Link>,
+              icon: { path: '/icons/square-small.svg', width: 14, height: 14 },
             },
             {
-              content: <Link href={getSubcategoryHref('5')}>РЕМОНТ ДВЕРЕЙ И КОРПУСА</Link>,
-              icon: {
-                path: '/icons/square-small.svg',
-                width: 14,
-                height: 14,
-              },
+              content: <Link href={getSubcategoryHref('5')}>Ремонт дверей и корпуса</Link>,
+              icon: { path: '/icons/square-small.svg', width: 14, height: 14 },
             },
             {
-              content: <Link href={getSubcategoryHref('6')}>ПРОЧИЕ УСЛУГИ</Link>,
-              icon: {
-                path: '/icons/square-small.svg',
-                width: 14,
-                height: 14,
-              },
+              content: <Link href={getSubcategoryHref('6')}>Прочие услуги</Link>,
+              icon: { path: '/icons/square-small.svg', width: 14, height: 14 },
             },
           ],
         },
         {
-          content: <Link href='https://google.com'>ВИДЫ ХОЛОДИЛЬНИКОВ</Link>,
-          icon: {
-            path: '/icons/chevron-down.svg',
-            width: 10,
-            height: 7,
-            position: 'after',
-          },
+          content: <Link href='https://google.com'>Виды холодильников</Link>,
+          icon: { path: '/icons/chevron-down.svg', width: 10, height: 7, position: 'after' },
           subItems: [
             {
-              content: <Link href='https://google.com'>РЕМОНТ БЫТОВЫХ ХОЛОДИЛЬНИКОВ</Link>,
-              icon: {
-                path: '/icons/square-small.svg',
-                width: 14,
-                height: 14,
-              },
+              content: <Link href='https://google.com'>Ремонт бытовых холодильников</Link>,
+              icon: { path: '/icons/square-small.svg', width: 14, height: 14 },
             },
             {
-              content: <Link href='https://google.com'>РЕМОНТ КОММЕРЧЕСКИХ ХОЛОДИЛЬНИКОВ</Link>,
-              icon: {
-                path: '/icons/square-small.svg',
-                width: 14,
-                height: 14,
-              },
+              content: <Link href='https://google.com'>Ремонт коммерческих холодильников</Link>,
+              icon: { path: '/icons/square-small.svg', width: 14, height: 14 },
             },
             {
-              content: <Link href='https://google.com'>РЕМОНТ СПЕЦИАЛЬНЫХ ХОЛОДИЛЬНЫХ УСТАНОВОК</Link>,
-              icon: {
-                path: '/icons/square-small.svg',
-                width: 14,
-                height: 14,
-              },
+              content: <Link href='https://google.com'>Ремонт специальных холодильных установок</Link>,
+              icon: { path: '/icons/square-small.svg', width: 14, height: 14 },
             },
             {
-              content: <Link href='https://google.com'>РЕМОНТ ПРОМЫШЛЕННЫХ СИСТЕМ ОХЛАЖДЕНИЯ</Link>,
-              icon: {
-                path: '/icons/square-small.svg',
-                width: 14,
-                height: 14,
-              },
+              content: <Link href='https://google.com'>Ремонт промышленных систем охлаждения</Link>,
+              icon: { path: '/icons/square-small.svg', width: 14, height: 14 },
             },
           ],
         },
         {
-          content: <Link href={`/${Routes.Prices}`}>ЦЕНЫ</Link>,
+          content: <Link href={`/${Routes.Prices}`}>Цены</Link>,
         },
         {
-          content: <Link href={`/${Routes.Payment}`}>ОПЛАТА</Link>,
+          content: <Link href={`/${Routes.Payment}`}>Оплата</Link>,
         },
         {
-          content: <Link href={`/${Routes.Reviews}`}>ОТЗЫВЫ</Link>,
+          content: <Link href={`/${Routes.Reviews}`}>Отзывы</Link>,
         },
         {
-          content: <Link href={`/${Routes.AboutUs}`}>О КОМПАНИИ</Link>,
+          content: <Link href={`/${Routes.AboutUs}`}>О компании</Link>,
         },
         {
-          content: <Link href={`/${Routes.Contacts}`}>КОНТАКТЫ</Link>,
+          content: <Link href={`/${Routes.Contacts}`}>Контакты</Link>,
         },
       ]} />
   </nav>
