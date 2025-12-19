f2f82d7c7484285931fc922c845538035e536e0d
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2025-12-19 23:16:33 +0300

    ui-kit: dialog: add props & styles

--- src/ui-kit/dialog/dialog.tsx
+++ src/ui-kit/dialog/dialog.tsx
@@ -1,21 +1,40 @@
-import { RefObject } from 'react';
+import { RefObject, useRef } from 'react';
 import Button from '../button/button';
 import styles from './dialog.module.scss';
+import composeClassName from '@helpers/compose-class-name';
+import useClickOutsideListener from '@hooks/use-click-outside-listener';
 
 export default function Dialog({
   children,
   dialogRef,
+  withCloseButton = true,
+  withBackdropClose = false,
+  withBackdropShadow = false,
+  transparentBackdrop = false,
 }: {
   children: React.ReactNode,
-  dialogRef: RefObject<HTMLDialogElement | null>
+  dialogRef: RefObject<HTMLDialogElement | null>,
+  withCloseButton?: boolean,
+  withBackdropClose?: boolean,
+  withBackdropShadow?: boolean,
+  transparentBackdrop?: boolean,
 }) {
-  return <dialog ref={dialogRef} className={styles.dialog}>
-    <div className={styles['dialog-content-wrapper']}>
-      <Button
+  const dialogContentWrapperRef = useRef<HTMLDivElement>(null);
+  useClickOutsideListener(dialogContentWrapperRef, () => {
+    withBackdropClose && dialogRef?.current?.close();
+  });
+  return <dialog ref={dialogRef} className={composeClassName(
+    styles.dialog,
+    !withCloseButton && styles['no-close-button'],
+    withBackdropShadow && styles['with-backdrop-shadow'],
+    transparentBackdrop && styles['transparent-backdrop'],
+  )}>
+    <div ref={dialogContentWrapperRef} className={styles['dialog-content-wrapper']}>
+      {withCloseButton && <Button
         customClass={styles['close-button']}
         icon={{ path: '/icons/cross.svg', width: 32, height: 32 }}
         onClick={() => dialogRef?.current?.close()}
-      />
+      />}
       <div className={styles['dialog-content']}>{children}</div>
     </div>
   </dialog>
