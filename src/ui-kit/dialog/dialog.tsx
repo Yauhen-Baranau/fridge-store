fc2e29545612ea5006e41da8ea1e28860f1e1cc7
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2025-12-20 13:06:12 +0300

    ui-kit: dialog: adjust styles for better positioning

--- src/ui-kit/dialog/dialog.tsx
+++ src/ui-kit/dialog/dialog.tsx
@@ -1,4 +1,6 @@
-import { RefObject } from 'react';
+'use client';
+
+import { CSSProperties, RefObject, useMemo, useRef } from 'react';
 import Button from '../button/button';
 import styles from './dialog.module.scss';
 import composeClassName from '@helpers/compose-class-name';
@@ -21,9 +23,20 @@ export default function Dialog({
   transparentBackdrop?: boolean,
   customPosition?: { top?: number, right?: number, bottom?: number, left?: number }
 }) {
-  // because dialog context's inner state needs to be updated
-  // when closing the dialog
   const { closeDialog } = useDialog();
+  const contentWrapperRef = useRef<HTMLDivElement>(null);
+  const contentWrapperStyles: CSSProperties = useMemo(() => {
+    if (!contentWrapperRef?.current || !customPosition) {
+      return {};
+    }
+    return {
+      position: 'absolute',
+      top: (customPosition.top !== undefined) ? `${customPosition.top}px` : 'unset',
+      right: (customPosition.right !== undefined) ? `${customPosition.right}px` : 'unset',
+      bottom: (customPosition.bottom !== undefined) ? `${customPosition.bottom}px` : 'unset',
+      left: (customPosition.left !== undefined) ? `${customPosition.left}px` : 'unset',
+    }
+  }, [customPosition, children]);
 
   return <dialog
     ref={dialogRef}
@@ -33,17 +46,14 @@ export default function Dialog({
       withBackdropShadow && styles['with-backdrop-shadow'],
       transparentBackdrop && styles['transparent-backdrop'],
     )}
-    style={{
-      top: (customPosition?.top !== undefined) ? `${customPosition.top}px` : 'unset',
-      right: (customPosition?.right !== undefined) ? `${customPosition.right}px` : 'unset',
-      bottom: (customPosition?.bottom !== undefined) ? `${customPosition.bottom}px` : 'unset',
-      left: (customPosition?.left !== undefined) ? `${customPosition.left}px` : 'unset',
-      width: customPosition ? 'fit-content' : 'revert',
-      height: customPosition ? 'fit-content' : 'revert',
-    }}
     onClick={() => withBackdropClose && closeDialog()}
   >
-    <div className={styles['dialog-content-wrapper']} onClick={e => e.stopPropagation()}>
+    <div
+      ref={contentWrapperRef}
+      className={styles['dialog-content-wrapper']}
+      onClick={e => e.stopPropagation()}
+      style={contentWrapperStyles}
+    >
       {withCloseButton && <Button
         customClass={styles['close-button']}
         icon={{ path: '/icons/cross.svg', width: 32, height: 32 }}
