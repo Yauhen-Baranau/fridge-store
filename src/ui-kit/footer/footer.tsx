c1179e3065b768b561cc385cf00daac11ea07c8c
Konstantin Loktev <konstantin.loktev.02@mail.ru>
2025-12-22 17:13:33 +0300

    responsive: footer (mobile)

--- src/ui-kit/footer/footer.tsx
+++ src/ui-kit/footer/footer.tsx
@@ -9,27 +9,45 @@ import Button from '@ui-kit/button/button';
 import { Routes } from '@constants/routes';
 import { useHrefHelper } from '@contexts/href-context';
 import useResponsive from '@hooks/use-responsive';
-import React from 'react';
+import React, { useMemo } from 'react';
+import Accordion from '@ui-kit/accordion/accordion';
 
 export default function Footer() {
   const { getPageHref, getCategoryHref } = useHrefHelper();
   const { isMobile } = useResponsive();
 
-  const footerColumnFactory = ({
+  const footerSectionFactory = ({
     title,
     content,
     footerContent,
+    isContacts = false,
   }: {
     title: string,
     content: React.ReactNode,
-    footerContent: React.ReactNode
+    footerContent: React.ReactNode,
+    isContacts?: boolean,
   }) => {
-    return <div className={styles['footer-column']}>
-      <h3 className={styles['footer-column-title']}>{title}</h3>
-      <div className={styles['footer-column-content']}>{content}</div>
-      <div className={styles['footer-column-footer']}>{footerContent}</div>
-    </div>
+    const titleJsx = <h3 className={styles['footer-column-title']}>{title}</h3>;
+    const contentJsx = <div className={styles['footer-column-content']}>{content}</div>;
+    const footerContentJsx = <div className={styles['footer-column-footer']}>{footerContent}</div>;
+
+    return (!isMobile || isContacts)
+      ? <div className={styles['footer-column']}>
+        {titleJsx}
+        {contentJsx}
+        {footerContentJsx}
+      </div>
+      : <Accordion
+        toggleAreaContent={titleJsx}
+        content={<>{contentJsx}{footerContentJsx}</>}
+        toggleAreaCustomClass={styles['footer-accordion-toggle-area']}
+        contentWrapperCustomClass={styles['footer-accordion-content-wrapper']}
+        buttonStyle='chevron'
+      />;
   };
+
+  const privacyPolicyLink = useMemo(() => <Link href='https://google.com'>Политика конфиденциальности</Link>, []);
+  const designedByLink = useMemo(() => <Link className={styles['designed-by-link']} href='https://www.instagram.com/kutsenko_olga1990?igsh=ZDlsY3JoNG0zeTh1'>Design by Volha Kutsenka</Link>, []);
 
   return <footer className={styles.footer}>
     {[
@@ -44,7 +62,7 @@ export default function Footer() {
           { content: <Link href={getPageHref(Routes.CommonFridgeProblems)}>Частые проблемы</Link> },
           { content: <Link href={getPageHref(Routes.Contacts)}>Контакты</Link> },
         ]} />,
-        footerContent: <>&copy; Copyright</>,
+        footerContent: !isMobile && <>&copy; Copyright</>,
       },
       {
         title: 'Услуги',
@@ -54,7 +72,7 @@ export default function Footer() {
           { content: <Link href='https://google.com'>Ремонт холодильников по модели</Link> },
           { content: <Link href='https://google.com'>Ремонт холодильников по городам</Link> },
         ]} />,
-        footerContent: <Link href='https://google.com'>Политика конфиденциальности</Link>,
+        footerContent: !isMobile && privacyPolicyLink,
       },
       {
         title: 'Контакты',
@@ -97,11 +115,11 @@ export default function Footer() {
                 height: 24,
               }
             },
-            { content: <Socials customClass={styles.socials} /> }
+            { content: !isMobile && <Socials customClass={styles.socials} /> }
           ]} />
         </address>,
         footerContent: <>
-          <Link className={styles['designed-by-link']} href='https://www.instagram.com/kutsenko_olga1990?igsh=ZDlsY3JoNG0zeTh1'>Design by Volha Kutsenka</Link>
+          {!isMobile ? designedByLink : privacyPolicyLink}
           <Button
             customClass={styles['scroll-up-button']}
             icon={{
@@ -111,8 +129,13 @@ export default function Footer() {
             }}
             onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
           />
-        </>
+        </>,
+        isContacts: true,
       }
-    ].map((params, index) => <React.Fragment key={index}>{footerColumnFactory(params)}</React.Fragment>)}
+    ].map((params, index) => <React.Fragment key={index}>{footerSectionFactory(params)}</React.Fragment>)}
+    {isMobile && <div className={styles['mobile-footer-lower-block']}>
+      <p>&copy; Copyright</p>
+      {designedByLink}
+    </div>}
   </footer>
 }
