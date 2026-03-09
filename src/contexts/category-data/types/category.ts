773c1feacaa7b66d507ac8e430296dfea1f2cdbc
Kanstantsin Loktsieu <konstantin.loktev@innowise.com>
2026-03-09 07:04:07 +0100

    add seotitle and seodescription fields to all categories/subcategories/services

--- src/contexts/category-data/types/category.ts
+++ src/contexts/category-data/types/category.ts
@@ -1,6 +1,8 @@
 export interface Category {
   id: string;
   label: string;
+  seoTitle: string;
+  seoDescription: string;
   description: Array<
     | { type: "paragraph"; content: string }
     | { type: "list"; title: string; items: Array<string> }
