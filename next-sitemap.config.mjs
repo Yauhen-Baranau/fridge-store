8c958adb4b97c9aeadc972d57c908d605d5fdee0
Yauhen-Baranau <66030848+Yauhen-Baranau@users.noreply.github.com>
2026-04-26 12:29:13 +0300

    Add next-sitemap package config and script

---
+++ next-sitemap.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: 'https://holodcentr.by',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  outDir: 'out',
+};
+
+export default config;
